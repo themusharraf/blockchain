312a1a5ab685e4460d9d12a539d78f85f05ae0fa
mrx <meibrohimov@gmail.com>
2024-11-22 11:26:43 +0500

    new feature

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ from tortoise import Tortoise
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_USERNAME = os.getenv('BOT_USERNAME')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
